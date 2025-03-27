e62e21775fa44c6a8903ef83fcdd42491353bad5
Bianca Mascolo <biancasmascolo@gmail.com>
2025-03-27 15:05:50 -0400

    Moved site files to root for GitHub Pages

---
+++ tailwind.config.js/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import NeuroNest from './NeuroNest';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <NeuroNest />
+  </React.StrictMode>
+);
